cfb2937e292fb0b03c503c69437747120d87a8a1
devsince2021 <dev.ryu2021@gmail.com>
2024-03-15 01:39:09 +0900

    Init: 프로젝트 생성

---
+++ src/main/java/com/coconut/user_spring/UserController.java
@@ -0,0 +1,13 @@
+package com.coconut.user_spring;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+
+  @GetMapping("/")
+  public String getCheck() {
+    return "Hi";
+  }
+}
