2a4614653e5674461c40786df0b5628344715617
devsince2021 <dev.ryu2021@gmail.com>
2024-03-18 00:01:17 +0900

    feat: code deploy에서 사용될 클린업 스크립트 생성

---
+++ scripts/docker-build-script.sh
@@ -0,0 +1,9 @@
+# docker buildx build --platform linux/amd64 -t user_service . --load
+
+#docker buildx build --platform linux/amd64 -t 767397898420.dkr.ecr.ap-northeast-2.amazonaws.com/user_service . --push
+
+# aws ecr get-login-password --region ap-northeast-2 | docker login --username AWS --password-stdin 767397898420.dkr.ecr.ap-northeast-2.amazonaws.com
+ docker buildx build --platform linux/amd64 -t 767397898420.dkr.ecr.ap-northeast-2.amazonaws.com/user_service:latest2 . --load
+# docker push 767397898420.dkr.ecr.ap-northeast-2.amazonaws.com/user_service
+
+
---
+++ scripts/local-docker-build-script.sh
@@ -0,0 +1,3 @@
+docker buildx build --platform linux/amd64 -t user_service . --load
+
+
