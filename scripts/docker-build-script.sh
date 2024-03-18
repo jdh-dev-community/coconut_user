6d2ab076ddcb8c132cebf1f5de82b78fc6f73fe3
devsince2021 <dev.ryu2021@gmail.com>
2024-03-19 02:02:03 +0900

    feat: 코드빌드에서 사용되는 buildspec.yml 정의

--- scripts/docker-build-script.sh
+++ scripts/docker-build-script.sh
@@ -3,7 +3,7 @@
 #docker buildx build --platform linux/amd64 -t 767397898420.dkr.ecr.ap-northeast-2.amazonaws.com/user_service . --push
 
 # aws ecr get-login-password --region ap-northeast-2 | docker login --username AWS --password-stdin 767397898420.dkr.ecr.ap-northeast-2.amazonaws.com
- docker buildx build --platform linux/amd64 -t 767397898420.dkr.ecr.ap-northeast-2.amazonaws.com/user_service:latest2 . --load
+# docker buildx build --platform linux/amd64 -t 767397898420.dkr.ecr.ap-northeast-2.amazonaws.com/user_service:latest . --push
 # docker push 767397898420.dkr.ecr.ap-northeast-2.amazonaws.com/user_service
 
 
